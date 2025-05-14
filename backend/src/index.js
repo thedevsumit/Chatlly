3aa49b5f527636e2c4242e63e5d4892e5beec8b3
Sumit <110337910+sumitopie122@users.noreply.github.com>
2025-05-14 22:55:29 +0530

    adde

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(process.env.PORT, () => {
   console.log("Running on port number:", process.env.PORT);
