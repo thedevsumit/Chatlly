b70263b6aad6b4d4ff77ae73b7827b8d83f14c58
Sumit <110337910+sumitopie122@users.noreply.github.com>
2025-05-15 10:10:25 +0530

    added

--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import { toast } from "react-toastify";
 import { io } from "socket.io-client"
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://chatlly.onrender.com" : "/";
 export const userAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
