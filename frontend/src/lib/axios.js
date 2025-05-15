48ebca02aeafc41779945d9a08878064ecfde729
Sumit <110337910+thedevsumit@users.noreply.github.com>
2025-05-15 10:54:51 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chatlly.onrender.com/api" : "/api",
+  baseURL: "https://chatlly.onrender.com/api",
   withCredentials: true,
 });
