72f3fc0607849fe4802e30132c9fa42f11c158ca
Sumit <110337910+sumitopie122@users.noreply.github.com>
2025-05-15 10:16:35 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://gochatlly.onrender.com/login",
   credentials: true,
 }));
 app.use(express.json());
